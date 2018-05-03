69ed764ae320e78568b5d2716c178730d3422139
dz <david@nodnod.net>
2018-05-02 22:06:13 -0400

    rename

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter Blog',
-    author: 'Kyle Mathews',
-    description: 'A starter blog demonstrating what Gatsby can do.',
-    siteUrl: 'https://gatsbyjs.github.io/gatsby-starter-blog/',
+    title: 'Bulk Closet',
+    author: 'DZ',
+    description: 'Go with it.',
+    siteUrl: 'http://bulkcloset.com',
   },
-  pathPrefix: '/gatsby-starter-blog',
+  pathPrefix: '/',
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
